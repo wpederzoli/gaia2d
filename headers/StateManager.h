6a0b2cb6314397997b0cb480a7328bb53e64cf1a
William Pederzoli <william.pederzoli@chromaticartworks.com>
2022-02-04 14:40:37 +0100

    State manager and game states subsystems

---
+++ headers/StateManager.h
@@ -0,0 +1,26 @@
+#ifndef STATEMANAGER_H
+#define STATEMANAGER_H
+
+#include <stack>
+#include "GameState.h"
+#include "Graphics.h"
+
+class GameState;
+
+class StateManager
+{
+    private:
+        std::stack<GameState*> states;
+    
+    public:
+        StateManager();
+        ~StateManager();
+
+        void addState(GameState* state);
+        void popState();
+        void update();
+        void draw(Graphics* g);
+        bool isEmpty();
+};
+
+#endif
