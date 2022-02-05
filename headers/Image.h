7521ddd06abf59f2d70b0703d8899c0c7ed53b71
William Pederzoli <wpederzoli@gmail.com>
2022-02-05 13:32:39 +0100

    Image class created

---
+++ headers/Image.h
@@ -0,0 +1,27 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include <SDL2/SDL_image.h>
+#include "Graphics.h"
+
+class Image
+{
+    private:
+        SDL_Texture* texture;
+        int width;
+        int height;
+        int posX;
+        int posY;
+
+    public:
+        Image();
+        ~Image();
+        
+        bool load(char const filePath[], Graphics* g);
+        void setFrame(int x, int y);
+        void draw(int x, int y, const Graphics* g);
+        void draw(int x, int y, int width, int height, const Graphics* g);
+        void drawSprite(int sx, int sy, int sw, int sh, int dx, int dy, int dw, int dh, Graphics* g);
+};
+
+#endif
